b185f50424bfb1c37d6a7d4a13abea20a423fc9b
Dawid Bednarczyk <dawid.bednarczyk@protonmail.com>
2025-05-05 22:17:06 +0200

    MapView and Marker

--- App.js
+++ App.js
@@ -1,11 +1,28 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import MapView, {Marker} from 'react-native-maps';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
+
+      <MapView style={styles.map}
+        initialRegion={{
+          latitude: 37.78825,
+          longitude: -122.4324,
+          latitudeDelta: 0.0922,
+          longitudeDelta: 0.0421,
+        }}
+      >
+      <Marker
+        coordinate={{ latitude: 52.2297, longitude: 21.0122 }}
+        title="Warszawa"
+      />
+      </MapView>
+
+      <Text>xd</Text>
     </View>
   );
 }
@@ -17,4 +34,8 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  map: {
+    width: '95%',
+    height: '50%'
+  }
 });
