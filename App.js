128ae075003972c4d8274543b884ef256fee8f48
Dawid Bednarczyk <dawid.bednarczyk@protonmail.com>
2025-05-10 20:37:41 +0200

    map with position of current pins + editable input textbox

--- App.js
+++ App.js
@@ -1,30 +1,32 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import MapView, {Marker} from 'react-native-maps';
+import { StyleSheet, Text, View, TextInput } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
+//import MapView, {Marker} from 'react-native-maps';
+import CustomMapView from './components/CustomMapView'
+//import { TextInput } from 'react-native-web';
+import React, { useState } from 'react';
 
 export default function App() {
   return (
-    <View style={styles.container}>
+    <SafeAreaView style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
 
-      <MapView style={styles.map}
-        initialRegion={{
-          latitude: 37.78825,
-          longitude: -122.4324,
-          latitudeDelta: 0.0922,
-          longitudeDelta: 0.0421,
-        }}
-      >
-      <Marker
-        coordinate={{ latitude: 52.2297, longitude: 21.0122 }}
-        title="Warszawa"
-      />
-      </MapView>
+      <CustomMapView style={styles.flex}></CustomMapView>
 
-      <Text>xd</Text>
-    </View>
+      <View style={styles.form}>
+        <Text> Nazwa</Text>
+        <TextInput style={ styles.textinput } editable />
+      </View>
+      
+      
+
+    </SafeAreaView>
   );
+}
+
+function onPress(){
+  alert("xDD");
 }
 
 const styles = StyleSheet.create({
@@ -35,7 +37,18 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
   map: {
-    width: '95%',
-    height: '50%'
+    flex: 1,
+  },
+  textinput: {
+    //flex: 1,
+    borderWidth: 1,
+    width: '95%'
+  },
+  mapContainer: {
+    flex: 1,
+  },
+  form: {
+    flex: 1,
+    width: '100%'
   }
 });
