5138af566965153ffd97c462628f3a7d15450815
Dawid Bednarczyk <dawid.bednarczyk@protonmail.com>
2025-05-18 00:28:02 +0200

    components first commit

--- App.js
+++ App.js
@@ -1,27 +1,37 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, TextInput } from 'react-native';
-import { SafeAreaView } from 'react-native-safe-area-context';
+import { FlatList, StyleSheet, Text, View, TextInput, Button, TouchableOpacity } from 'react-native';
+import { SafeAreaProvider, SafeAreaView, useSafeAreaInsets } from 'react-native-safe-area-context';
 //import MapView, {Marker} from 'react-native-maps';
 import CustomMapView from './components/CustomMapView'
 //import { TextInput } from 'react-native-web';
 import React, { useState } from 'react';
+import CustomFlatList from './components/CustomFlatList';
 
 export default function App() {
+
+  const [currentCoords, setCurrentCoords] = useState(null);
+
   return (
-    <SafeAreaView style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <SafeAreaProvider style={[styles.main_container, styles.background]}>
       <StatusBar style="auto" />
+      <View style={[styles.container, styles.background]}>
+        <Text style={styles.textIntroduction}>Moje ulubione miejsca</Text>
+        <CustomMapView style={styles.mapContainer} onPinChange={setCurrentCoords} ></CustomMapView>
 
-      <CustomMapView style={styles.flex}></CustomMapView>
-
-      <View style={styles.form}>
-        <Text> Nazwa</Text>
-        <TextInput style={ styles.textinput } editable />
+        {/* <View style={styles.form}> */}
+          {/* <Text> Nazwa</Text>
+          <TextInput style={ styles.textinput } editable />
+          <Text>{currentCoords ? `${currentCoords.longitude} ${currentCoords.latitude}`: ``}
+          </Text> */}
+        {/* </View> */}
+      </View>
+      <View style={[styles.listPanel,  ]}>
+        <CustomFlatList style={styles.list}/>
       </View>
       
-      
+      {/* <Button title="siema"/> */}
 
-    </SafeAreaView>
+    </SafeAreaProvider>
   );
 }
 
@@ -30,14 +40,37 @@ function onPress(){
 }
 
 const styles = StyleSheet.create({
-  container: {
+  background: {
+    backgroundColor: '#FFF6F1',
+  },
+  main_container:{
     flex: 1,
-    backgroundColor: '#fff',
+    flexDirection: 'column',
+    justifyContent: 'flex-start',
+    // backgroundColor: '#FFF6F1',
+    textAlign:'center',
     alignItems: 'center',
+    //alignContent: 'center',
+    paddingTop: '5%'
+  },
+  container: {
+    flex: 4,
+    width: '100%',
+    backgroundColor: '#fff',
+    // alignSelf: 1,
+    alignItems: 'center',
+    // justifyContent: 'center',
     justifyContent: 'center',
+    // alignContent: 'center'
+    marginBottom: '5%',
+    // borderWidth: 1,
+    // borderRadius: 3
   },
   map: {
-    flex: 1,
+    // flex: 1,
+    // backgroundColor: '#fff',
+    // alignItems: 'center',
+    // justifyContent: 'center',
   },
   textinput: {
     //flex: 1,
@@ -46,9 +79,40 @@ const styles = StyleSheet.create({
   },
   mapContainer: {
     flex: 1,
+    borderRadius: 22,
+    evaluate: 3,
+    overflow: 'hidden'
   },
   form: {
-    flex: 1,
-    width: '100%'
+    // flex: 1,
+    width: '100%',
+    marginBottom: '1px',
+  },
+  listPanel: {
+    flex: 2,
+    // borderWidth: 1,
+    // borderRadius: 22,
+    // evaluate: 3,
+    width: '100%',
+    alignContent: "center",
+    alignItems: 'center',
+    marginLeft: '1px',
+    marginRight: '1px',
+  },
+  list: {
+    flex: 2,
+    // borderWidth: 1,
+    borderRadius: 22,
+    evaluate: 3,
+    width: '100%',
+    alignContent: "center",
+    alignItems: 'center',
+    marginLeft: '1px',
+    marginRight: '1px',
+  },
+  textIntroduction: {
+    fontSize: 30,
+    textAlign: 'center',
+    padding: 20
   }
 });
